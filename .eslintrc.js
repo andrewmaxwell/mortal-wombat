3a78b21d59fa6df49c825df4ec23af1436eb57b2
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2022-04-05 09:10:23 -0500

    added linting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {jsx: true},
+  },
+  env: {browser: true, es6: true, node: true, 'jest/globals': true},
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['prettier', 'react', 'jest'],
+  rules: {
+    'prettier/prettier': ['warn', {singleQuote: true, bracketSpacing: false}],
+    'dot-notation': 'warn',
+    'quote-props': ['warn', 'as-needed'],
+    'arrow-body-style': ['warn', 'as-needed'],
+    'object-shorthand': 'warn',
+    'no-use-before-define': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {react: {version: 'detect'}},
+};
