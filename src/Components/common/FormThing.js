480b894f2706f3bb251643c098027a85fff04dc8
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2022-06-08 13:20:38 -0500

    Make monaco not lazy load?

--- src/Components/common/FormThing.js
+++ src/Components/common/FormThing.js
@@ -1,6 +1,4 @@
-import {lazy, Suspense} from 'react';
-
-const Editor = lazy(() => import('@monaco-editor/react'));
+import Editor from '@monaco-editor/react';
 
 const TextField = ({value = '', onChange}) => (
   <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />
@@ -35,16 +33,14 @@ const Select = ({value, onChange, options}) => (
 
 // TODO: add code validation
 const Code = ({value, onChange}) => (
-  <Suspense fallback="Loading...">
-    <Editor
-      height={400}
-      width={800}
-      theme="vs-dark"
-      defaultLanguage="javascript"
-      value={value}
-      onChange={onChange}
-    />
-  </Suspense>
+  <Editor
+    height={400}
+    width={800}
+    theme="vs-dark"
+    defaultLanguage="javascript"
+    value={value}
+    onChange={onChange}
+  />
 );
 
 const inputs = {
